043bc4d19711629c0a5f0546831bb2758c21c6d4
Oliver <wzy@umich.edu>
2013-11-28 15:30:43 -0500

    game logic completely removed from client

--- game/Board.java
+++ game/Board.java
@@ -133,7 +133,6 @@ public class Board extends JPanel implements Commons {
         public void run() {
             
             player.move();
-//            checkCollision();
             repaint();
 
         }
@@ -145,96 +144,4 @@ public class Board extends JPanel implements Commons {
     }
 
 
-    public void checkCollision() {
-
-        if (ball.getRect().getMaxY() > Commons.BOTTOM) {
-            stopGame();
-        }
-
-        for (int i = 0, j = 0; i < 30; i++) {
-            if (bricks[i].isDestroyed()) {
-                j++;
-            }
-            if (j == 30) {
-                message = "Victory";
-                stopGame();
-            }
-        }
-
-        if ((ball.getRect()).intersects(player.getRect())) {
-
-            int paddleLPos = (int)player.getRect().getMinX();
-            int ballLPos = (int)ball.getRect().getMinX();
-
-            int first = paddleLPos + 8;
-            int second = paddleLPos + 16;
-            int third = paddleLPos + 24;
-            int fourth = paddleLPos + 32;
-
-            if (ballLPos < first) {
-                ball.setXDir(-1);
-                ball.setYDir(-1);
-            }
-
-            if (ballLPos >= first && ballLPos < second) {
-                ball.setXDir(-1);
-                ball.setYDir(-1 * ball.getYDir());
-            }
-
-            if (ballLPos >= second && ballLPos < third) {
-                ball.setXDir(0);
-                ball.setYDir(-1);
-            }
-
-            if (ballLPos >= third && ballLPos < fourth) {
-                ball.setXDir(1);
-                ball.setYDir(-1 * ball.getYDir());
-            }
-
-            if (ballLPos > fourth) {
-                ball.setXDir(1);
-                ball.setYDir(-1);
-            }
-
-
-        }
-
-
-        for (int i = 0; i < 30; i++) {
-            if ((ball.getRect()).intersects(bricks[i].getRect())) {
-
-                int ballLeft = (int)ball.getRect().getMinX();
-                int ballHeight = (int)ball.getRect().getHeight();
-                int ballWidth = (int)ball.getRect().getWidth();
-                int ballTop = (int)ball.getRect().getMinY();
-
-                Point pointRight =
-                    new Point(ballLeft + ballWidth + 1, ballTop);
-                Point pointLeft = new Point(ballLeft - 1, ballTop);
-                Point pointTop = new Point(ballLeft, ballTop - 1);
-                Point pointBottom =
-                    new Point(ballLeft, ballTop + ballHeight + 1);
-
-                if (!bricks[i].isDestroyed()) {
-                    if (bricks[i].getRect().contains(pointRight)) {
-                        ball.setXDir(-1);
-                    }
-
-                    else if (bricks[i].getRect().contains(pointLeft)) {
-                        ball.setXDir(1);
-                    }
-
-                    if (bricks[i].getRect().contains(pointTop)) {
-                        ball.setYDir(1);
-                    }
-
-                    else if (bricks[i].getRect().contains(pointBottom)) {
-                        ball.setYDir(-1);
-                    }
-
-                    bricks[i].setDestroyed(true);
-                }
-            }
-        }
-    }
 }
